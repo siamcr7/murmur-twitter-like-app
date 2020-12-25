40ed84c45f0ed252b50e5cc2f85a3764da75358c
Jamil <siam_cr7@yahoo.com>
2020-12-25 14:48:29 +0600

    Node - Express: - Setup Basic Get API endpoint; - Added TS-Lint.json file;

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,34 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 
 const app = express();
 const PORT = 8000;
-app.get("/", (req, res) => res.send("Express + TypeScript Server. Tested Edited"));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES FOR OUR API
+// =============================================================================
+const router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', (req, res) => {
+  res.json({ message: 'hooray! welcome to our api!' });
 });
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(PORT);
+
+
+// tslint:disable-next-line: no-console
+console.log('Magic happens on port ' + PORT);
