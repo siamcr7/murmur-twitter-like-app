cc1c7837ca1c84a0518a33de5311a66b05b6146d
Jamil <siam_cr7@yahoo.com>
2020-12-26 11:27:15 +0600

    Finished setting up initial db connection to MySQL

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mysql from "mysql";
 
+// TODO: move the hard-coded values to appsettings.json file
 
 const app = express();
 const PORT = 8000;
@@ -10,13 +12,48 @@ const PORT = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// ======================================
+// DB Connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'rootpass',
+  database: 'murmur_schema',
+  port: 3306,
+  insecureAuth: true,
+});
+
+connection.connect();
+
+
 // ROUTES FOR OUR API
 // =============================================================================
-const router = express.Router();              // get an instance of the express Router
+const router = express.Router();         // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', (req, res) => {
-  res.json({ message: 'hooray! welcome to our api!' });
+router.get('/user', (req, res) => {
+
+  connection.query('SELECT * FROM Users', (err, rows, fields) => {
+    if (err) {
+      // console.log('See Err ', err);
+      // throw err;
+    }
+
+    res.status(500).send({
+      error: `Internal Server Error`
+    });
+
+    // res.status(200).json({
+    //   message: "hooray! welcome to our api!",
+    //   rows,
+    //   fields
+    // });
+
+    // console.log('The solution is: ', rows[0].solution);
+  });
+
+
+  // res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes for our API will happen here
