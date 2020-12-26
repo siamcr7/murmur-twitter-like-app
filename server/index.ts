d54aede281e07888d0045b1ae8c87e89ca8b8ff3
Jamil <siam_cr7@yahoo.com>
2020-12-26 13:43:17 +0600

    Initial setup of Angular is done

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,13 @@ import mysql from "mysql";
 const app = express();
 const PORT = 8000;
 
+// cors setting
+app.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,15 +46,15 @@ router.get('/user', (req, res) => {
       // throw err;
     }
 
-    res.status(500).send({
-      error: `Internal Server Error`
-    });
-
-    // res.status(200).json({
-    //   message: "hooray! welcome to our api!",
-    //   rows,
-    //   fields
+    // res.status(500).send({
+    //   error: `Internal Server Error`
     // });
+
+    res.status(200).json({
+      // message: "hooray! welcome to our api!",
+      // rows: toCamel(rows)
+      Rows: rows
+    });
 
     // console.log('The solution is: ', rows[0].solution);
   });
@@ -57,6 +64,23 @@ router.get('/user', (req, res) => {
 });
 
 // more routes for our API will happen here
+
+// TODO: move this to a middle ware file toCamel
+app.use((req, res, next) => {
+  // res = toCamel(res);
+  // console.log('See :', res.status(200).json);
+
+  const oldSend = res.json;
+  res.json = (data) => {
+    // console.log('See :', data); // do something with the data
+    const convertData = toCamel(data);
+    // console.log('See Converted :', convertData); // do something with the data
+    res.json = oldSend; // set function back to avoid the 'double-send' loop
+    return res.json(convertData); // just call as normal with data
+  };
+
+  next();
+});
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
@@ -69,3 +93,32 @@ app.listen(PORT);
 
 // tslint:disable-next-line: no-console
 console.log('Magic happens on port ' + PORT);
+
+// TODO: move to another file
+function toCamel(o: any) {
+  let newO: any;
+  let origKey;
+  let newKey;
+  let value;
+  if (o instanceof Array) {
+    return o.map((innerValue) => {
+        if (typeof innerValue === "object") {
+          innerValue = toCamel(innerValue)
+        }
+        return innerValue
+    })
+  } else {
+    newO = {}
+    for (origKey in o) {
+      if (o.hasOwnProperty(origKey)) {
+        newKey = (origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey).toString()
+        value = o[origKey]
+        if (value instanceof Array || (value !== null && value.constructor === Object)) {
+          value = toCamel(value)
+        }
+        newO[newKey] = value
+      }
+    }
+  }
+  return newO
+}
