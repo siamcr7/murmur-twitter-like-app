4aedcdb70c702783fbe56df492030c36482bd471
Jamil <siam_cr7@yahoo.com>
2020-12-27 20:46:23 +0600

    Implemented pagination in murmur tweets;

--- murmur-ui/src/app/components/timeline/timeline.component.ts
+++ murmur-ui/src/app/components/timeline/timeline.component.ts
@@ -1,51 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Self } from '@angular/core';
 import { BehaviorSubject, forkJoin, Observable } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 import { MurmurTweetDetails } from 'src/app/shared/models/murmur-tweet-details.model';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { MurmurTweetsStateService } from 'src/app/shared/services/murmur-tweets-state.service';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+  styleUrls: ['./timeline.component.css'],
+  providers: [ MurmurTweetsStateService ]
 })
 export class TimelineComponent implements OnInit {
 
-  private trigger$ = new BehaviorSubject<boolean>(false);
+  totalMurmurs$ = this.murmurStateService.totalMurmurs$;
+  readonly murmurTweetDetails$: Observable<MurmurTweetDetails[]> = this.murmurStateService.murmurTweetDetails$;
 
-  readonly murmurTweetDetails$: Observable<MurmurTweetDetails[]> = this.trigger$.pipe(
-    mergeMap(() => {
-      return forkJoin({
-        users: this.apiService.getUsers$(),
-        posts: this.apiService.getPosts$(this.authService.getEnsuredLoggedInUserId()),
-        likedPosts: this.apiService.getLikedPosts$(this.authService.getEnsuredLoggedInUserId())
-      });
-    }),
-    map(res => {
-      const userDisplayNameMappedToId = new Map<number, string>([
-        ...res.users.map(user => [user.id, user.name] as [number, string])
-      ]);
-
-      const likedPostIdsMapped = new Map<number, boolean>([
-        ...res.likedPosts.map(postId => [postId, true] as [number, boolean])
-      ]);
-
-      return res.posts.map(post => {
-        return {
-          canDelete: post.userId === this.authService.getEnsuredLoggedInUserId(),
-          content: post.content,
-          isLiked: likedPostIdsMapped.has(post.id as number),
-          likeCount: post.likeCount ?? 0,
-          userDisplayName: userDisplayNameMappedToId.get(post.userId),
-          userId: post.userId,
-          id: post.id
-        } as MurmurTweetDetails;
-      })
-    })
-  );
-
-  constructor(private apiService: ApiService, private authService: AuthService) { }
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService,
+    @Self() private murmurStateService: MurmurTweetsStateService) { }
 
   ngOnInit(): void {
   }
@@ -55,22 +30,24 @@ export class TimelineComponent implements OnInit {
       return;
     }
     const id = this.authService.getEnsuredLoggedInUserId();
-    this.apiService.addPost$({ content, userId: id }).subscribe(() => this.triggerChange());
+    this.apiService.addPost$({ content, userId: id }).subscribe(() => this.murmurStateService.triggerChange());
   }
 
   deletePost(postId: any): void {
-    this.apiService.deletePost$(+postId).subscribe(() => this.triggerChange());
+    this.apiService.deletePost$(+postId).subscribe(() => this.murmurStateService.triggerChange());
   }
 
   likePost(postId: any): void {
-    this.apiService.addPostLike$(this.authService.getEnsuredLoggedInUserId(), +postId).subscribe(() => this.triggerChange());
+    this.apiService.addPostLike$(this.authService.getEnsuredLoggedInUserId(), +postId)
+      .subscribe(() => this.murmurStateService.triggerChange());
   }
 
   unlikePost(postId: any): void {
-    this.apiService.deletePostLike$(this.authService.getEnsuredLoggedInUserId(), +postId).subscribe(() => this.triggerChange());
+    this.apiService.deletePostLike$(this.authService.getEnsuredLoggedInUserId(), +postId)
+      .subscribe(() => this.murmurStateService.triggerChange());
   }
 
-  private triggerChange(): void {
-    this.trigger$.next(!this.trigger$.getValue());
+  pagination(skip: any): void {
+    this.murmurStateService.pagination(skip);
   }
 }
